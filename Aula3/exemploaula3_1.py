81a61c665cfa209e334249142d10119f6e926463
douglascursino <douglascursino@hotmail.com>
2025-03-14 19:46:40 -0300

    Upload Aula3

---
+++ Aula3/exemploaula3_1.py
@@ -0,0 +1,7 @@
+a = input("Entre com seu nome:")
+b = int(input("Entre com a sua idade:"))
+print("O seu nome é", a, "e a sua idade é", b)
+if b>18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")    
