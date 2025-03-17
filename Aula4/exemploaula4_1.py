492fbf211aae5943f04e25720fd09d26109b270a
Aluno <douglascursino@hotmail.com>
2025-03-17 18:38:07 -0300

    Aula4

---
+++ Aula4/exemploaula4_1.py
@@ -0,0 +1,10 @@
+for i in range(10):
+    print("Vo valor i é atualmente: ", i)
+
+for i in range(1,6):
+    print(i)
+
+for i in range(2,8,3):
+    print("O valor de i é: ", i)
+            
+        
