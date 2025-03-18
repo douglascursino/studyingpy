1763e6fab6ce5c0dc9d9d2c8b7da727290d927c3
Aluno <douglascursino@hotmail.com>
2025-03-18 18:25:52 -0300

    Aula5

---
+++ Aula5/exemploaula5_1.py
@@ -0,0 +1,3 @@
+numeros = [10,5,7,2,1]
+numeros[1] = numeros[4]
+print("Novo conteúdo da lista", numeros)
---
+++ Aula5/exemploaula5_2.py
@@ -0,0 +1,9 @@
+#Criando uma lista
+minha_lista = [1,2,3,4,5]
+
+# Usando a fução len() para contar
+#o numero de elementos na lista
+tamanho_lista = len(minha_lista)
+
+#Imprimindo o resultado
+print("O tamanho da lista é:", tamanho_lista)
