f8d7735a9c66890f56a4e82e749865fbce79382e
Aluno <douglascursino@hotmail.com>
2025-03-19 18:59:48 -0300

    Aula5

---
+++ Aula5/lista11.py
@@ -0,0 +1,6 @@
+minha_lista = [] # Criando uma lista vazia.
+
+for i in range(5):
+    minha_lista.append(i+1) # nao irá iniciar com zero
+
+print(minha_lista)
---
+++ Aula5/lista12.py
@@ -0,0 +1,8 @@
+minha_lista = [10,1,8,3,5]
+total = 0
+
+for i in range(len(minha_lista)):
+    total = total + minha_lista[i]
+
+print(total)
+
